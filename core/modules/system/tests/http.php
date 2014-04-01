507355508073d672f1cadb156903733296869be4
sun <sun@unleashedmind.com>
2014-04-01 19:37:34 +0200

    Fixed bogus HTTP → HTTPS change in http.php test front-controller.

--- core/modules/system/tests/http.php
+++ core/modules/system/tests/http.php
@@ -18,11 +18,11 @@ $is_http_mock = !empty($_SERVER['HTTPS']);
 
 // Change to HTTP.
 $_SERVER['HTTPS'] = NULL;
+ini_set('session.cookie_secure', FALSE);
 foreach ($_SERVER as &$value) {
   $value = str_replace('core/modules/system/tests/http.php', 'index.php', $value);
-  $value = str_replace('http://', 'https://', $value);
+  $value = str_replace('https://', 'http://', $value);
 }
-ini_set('session.cookie_secure', FALSE);
 
 $request = Request::createFromGlobals();
 $kernel = DrupalKernel::createFromRequest($request);
