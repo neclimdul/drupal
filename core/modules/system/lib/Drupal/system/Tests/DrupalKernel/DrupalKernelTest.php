0bcc8c786bcef4b739d6acf982c80969164a55fe
sun <sun@unleashedmind.com>
2014-05-07 23:34:46 +0200

    Reverted bogus test method visibility changes.

--- core/modules/system/lib/Drupal/system/Tests/DrupalKernel/DrupalKernelTest.php
+++ core/modules/system/lib/Drupal/system/Tests/DrupalKernel/DrupalKernelTest.php
@@ -30,7 +30,7 @@ class DrupalKernelTest extends DrupalUnitTestBase {
     );
   }
 
-  public function setUp() {
+  function setUp() {
     // DrupalKernel relies on global $config_directories and requires those
     // directories to exist. Therefore, create the directories, but do not
     // invoke DrupalUnitTestBase::setUp(), since that would set up further
@@ -80,7 +80,7 @@ class DrupalKernelTest extends DrupalUnitTestBase {
   /**
    * Tests DIC compilation.
    */
-  public function testCompileDIC() {
+  function testCompileDIC() {
     // @todo: write a memory based storage backend for testing.
     $modules_enabled = array(
       'system' => 'system',
